2e4f05df3905ba2eebfc8f399771cd8d0dc60c91
wulunyi <wulunyi@weidian.com>
2016-12-10 18:38:05 +0800

    完成

--- index.js
+++ index.js
@@ -2,10 +2,3 @@
 
 import preview from './src/pre-view';
 window.preview = preview;
-
-var srcArr = [
-	'https://si.geilicdn.com/vshop208863409-1452764719638-88647-s1.jpg',
-	'http://7tszky.com1.z0.glb.clouddn.com/FmN8zwJyeEi8NCT3SMuSQ9YEAoFl'
-];
-
-preview.show(srcArr, 0);
